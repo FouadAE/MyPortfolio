187db9b397b919f88c1a197ca2d958a11dfdd636
fouad ait elhaj <aitelhajfouad@gmail.com>
2022-08-12 18:46:33 +0100

    change github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
             
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/fouadae/Portfolio"
+                href="https://github.com/fouadae/"
                 target="_blank"
                 className="fork-btn-inner"
               >
