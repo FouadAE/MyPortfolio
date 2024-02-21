827d42504f4c1c4b2a38b485f5ae1306d7d4e415
FouadAE <fouad.aitelhaj@edu.uca.ma>
2024-02-21 21:45:24 +0100

    modify description

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,10 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Fouad Ait Elhaj </span>
             from <span className="purple"> Attaouia, Morroco .</span>
-            <br />I have successfully completed my master's degree in Data Science & Machine Learning from FSTM Marrakech. Currently, I am gaining valuable experience as a Junior Full Stack Developer, focusing on Chrome extension development. My work involves utilizing OpenAI API, Langchain, and vector databases such as Pinecone to enhance the functionality and capabilities of these extensions.
+            <br />Data Science and Decision Support Master's graduate with a passion for using data to generate
+actionable insights and help businesses make informed decisions. With a strong background in
+statistics, programming, and data analysis, I am seeking a job
+opportunity where I can put my technical skills to the test and contribute to concrete projects.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
